9dacb0eb77430b6aaa31658d7dc0b5fa291aa6d4
emijsho <145356364+emijsho@users.noreply.github.com>
2023-12-10 17:42:11 +0000

    updated user experience

--- src/script.js
+++ src/script.js
@@ -11,11 +11,12 @@ function generateQuote(event) {
   event.preventDefault();
   let instructionInput = document.querySelector("#user-instructions");
   let apiKey = "494f3181eb1oe9bfae0t4f2214913d5b";
-  let prompt = `Please Generate a known film quote about ${instructionInput.value}`;
-  let context = "";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=
-  ${context}&key=${apiKey}
+  let prompt = `Please generate a known film quote about ${instructionInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}
 `;
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">Generating a Film Quote for you about ${instructionInput.value}...</div>`;
 
   console.log("generating quote");
   console.log(`prompt: ${prompt}`);
